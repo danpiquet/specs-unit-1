36a673fcc0dd52530f4d82aec115b71073e8e258
Dan Piquet <dan.piquet@gmail.com>
2022-12-17 10:16:23 -0700

    fixed image source issue

--- src/App.js
+++ src/App.js
@@ -39,7 +39,6 @@ function App() {
         setTimeout(() => {
           handleClick();
         }, 5000);
-
         setSquares([
           "POOP",
           "POOP",
--- src/Square.jsx
+++ src/Square.jsx
@@ -23,7 +23,7 @@ const Square = (props) => {
         <img src="https://i.ibb.co/jTN7qcs/X.png" alt="X" />
       ) : props.squareValue === "POOP" ? (
         <img
-          src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c39c.png"
+          src="https://www.freeiconspng.com/thumbs/poop-emoji-png/poop-png-emoji-island-0.png"
           alt="POOP"
         />
       ) : (
